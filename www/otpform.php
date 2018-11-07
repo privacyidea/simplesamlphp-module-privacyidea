0a55b30112307b318b1d4f7135bac9f77395d700
Micha Preußer <mail@mpreusser.de>
2018-11-07 14:03:48 +0100

    Added auth proc filters

---
+++ www/otpform.php
@@ -0,0 +1,29 @@
+<?php
+	try{
+		$authStateId = $_REQUEST['StateId'];
+		$state = SimpleSAML_Auth_State::loadState($authStateId, 'privacyidea:privacyidea:init');
+	} catch (Exception $e){
+	}
+
+	if(isset($_POST['OTP'])) {
+	    try {
+		    if(SimpleSAML\Module\privacyidea\Auth\Process\privacyidea::authenticate($state, $_POST['OTP'])) {
+			    SimpleSAML_Auth_State::saveState($state, 'privacyidea:privacyidea:init');
+			    SimpleSAML_Auth_ProcessingChain::resumeProcessing($state);
+		    } else {
+			    echo "Wrong OTP";
+		    }
+        } catch (Exception $e){
+	        echo $e;
+        }
+	}
+
+	$cfg = SimpleSAML_Configuration::getInstance();
+	$tpl = new SimpleSAML_XHTML_Template($cfg, 'privacyidea:loginform.php');
+	$trans = $tpl->getTranslator();
+	$tpl->data['otpform'] = true;
+	$tpl->data['params'] = array('StateId' => $authStateId);
+	$tpl->data['error'] = ($error) ? $trans->t($error) : false;
+	$tpl->show();
+
+	?>
