fe0d7be662447738d5be92e7b39f59c8d8d64ea8
Micha Preußer <mail@mpreusser.de>
2018-12-12 15:34:15 +0100

    Started to enroll tokens

--- lib/Auth/Process/tokenEnrollment.php
+++ lib/Auth/Process/tokenEnrollment.php
@@ -1,7 +1,39 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: micha
- * Date: 12.12.18
- * Time: 15:24
+ * If a user does not have a token, a new one could be enrolled.
+ * @author Micha Preußer <micha.preusser@netknights.it>
  */
+
+class sspmod_privacyIDEA_Auth_Process_tokenEnrollment extends SimpleSAML_Auth_ProcessingFilter {
+
+	/**
+	 * The administrator has to configure which token type the user should enroll.
+	 * @var String
+	 */
+	private $tokenType;
+
+	/**
+	 * The username for the service account
+	 * @var String
+	 */
+	private $serviceAccount;
+
+	/**
+	 * The password for the service account
+	 * @var String
+	 */
+	private $servicePass;
+
+	public function __construct( array $config, $reserved ) {
+		parent::__construct( $config, $reserved );
+		$cfg = SimpleSAML_Configuration::loadFromArray($config, 'privacyidea:tokenEnrollment');
+		$this->tokenType = $cfg->getString('tokenType', 'totp');
+		$this->serviceAccount = $cfg->getString('serviceAccount', 'service');
+		$this->servicePass = $cfg->getString('servicePass', 'service');
+	}
+
+	public function process( &$state) {
+		
+	}
+
+}
