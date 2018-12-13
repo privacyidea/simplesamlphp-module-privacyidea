6cd1804ad4631d300b0469b47171c38349777cd4
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2018-12-13 10:53:16 +0100

    tune some wording

--- lib/Auth/Process/checkClientIP.php
+++ lib/Auth/Process/checkClientIP.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * This authproc filter enables the possibility to check which IP the client has.
- * If it is wanted, the IDP is able to disable 2FA for special clients.
- * For example a user, which is located in the local area network, does not need 2FA
+ * This Auth Proc Filter allows to check the IP of the client and to control the 
+ * two-factor authentication of the following privacyIDEA Auth Proc Filter depending on the client IP.
+ * For example, it can be used to configure that a user does not need to provide a 
+ * second factor when logging in from the local network.
  * @author Micha Preußer <micha.preusser@netknights.it>
  */
 
