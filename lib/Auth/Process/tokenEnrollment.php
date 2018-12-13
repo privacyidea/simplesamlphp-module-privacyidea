eeae8280667aa9ceee5a4547c3dfb95d8aeac4b3
Micha Preußer <mail@mpreusser.de>
2018-12-13 11:10:30 +0100

    Saved changes

--- lib/Auth/Process/tokenEnrollment.php
+++ lib/Auth/Process/tokenEnrollment.php
@@ -1,10 +1,17 @@
 <?php
 /**
  * If a user does not have a token, a new one could be enrolled.
+ * This one will be generated with a QR-code, which can be scanned with a mobile device.
+ * For that you'll need an application like 'privacyIDEA Authenticator' or 'Google Authenticator'
  * @author Micha Preußer <micha.preusser@netknights.it>
  */
 
 class sspmod_privacyIDEA_Auth_Process_tokenEnrollment extends SimpleSAML_Auth_ProcessingFilter {
+
+	/**
+	 *
+	 */
+	private $privacyIDEA_URL;
 
 	/**
 	 * The administrator has to configure which token type the user should enroll.
@@ -30,10 +37,17 @@ class sspmod_privacyIDEA_Auth_Process_tokenEnrollment extends SimpleSAML_Auth_Pr
 		$this->tokenType = $cfg->getString('tokenType', 'totp');
 		$this->serviceAccount = $cfg->getString('serviceAccount', 'service');
 		$this->servicePass = $cfg->getString('servicePass', 'service');
+
+		$cfg = SimpleSAML_Configuration::loadFromArray($config, 'privacyidea:privacyidea');
+		$this->privacyIDEA_URL = $cfg->getString('privacyideaserver');
 	}
 
-	public function process( &$state) {
+	public function process( &$state ) {
 		
+	}
+
+	public function checkIfUserHasToken( &$state ) {
+
 	}
 
 }
