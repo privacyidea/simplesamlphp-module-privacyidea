71840972a21785a12532a1230c8d6024774b70fd
lukasmatusiewicz <77617779+lukasmatusiewicz@users.noreply.github.com>
2024-03-12 15:18:43 +0100

    Update Utils.php

--- lib/Auth/Utils.php
+++ lib/Auth/Utils.php
@@ -79,7 +79,7 @@ class Utils
                     $response = $pi->validateCheck($username, "", $transactionID, $headers);
                 }
             }
-            catch (\Exception $e)
+            catch (\PIBadRequestException $e)
             {
                 Utils::handlePrivacyIDEAException($e, $state);
             }
@@ -98,7 +98,7 @@ class Utils
                 {
                     $response = $pi->validateCheckU2F($username, $transactionID, $u2fSignResponse, $headers);
                 }
-                catch (\Exception $e)
+                catch (\PIBadRequestException $e)
                 {
                     Utils::handlePrivacyIDEAException($e, $state);
                 }
@@ -119,7 +119,7 @@ class Utils
                 {
                     $response = $pi->validateCheckWebAuthn($username, $transactionID, $webAuthnSignResponse, $origin, $headers);
                 }
-                catch (\Exception $e)
+                catch (\PIBadRequestException $e)
                 {
                     self::handlePrivacyIDEAException($e, $state);
                 }
@@ -131,7 +131,7 @@ class Utils
             {
                 $response = $pi->validateCheck($username, $formParams["otp"], $transactionID, $headers);
             }
-            catch (\Exception $e)
+            catch (\PIBadRequestException $e)
             {
                 self::handlePrivacyIDEAException($e, $state);
             }
@@ -240,36 +240,36 @@ class Utils
         if (!empty($config['privacyideaServerURL']))
         {
             $pi = new PrivacyIDEA("simpleSAMLphp", $config['privacyideaServerURL']);
-            $pi->logger = new PILogger();
+            $pi->setLogger(new PILogger());
 
             if (array_key_exists('sslVerifyHost', $config) && !empty($config['sslVerifyHost']))
             {
-                $pi->sslVerifyHost = $config['sslVerifyHost'] !== "false";
+                $pi->setSSLVerifyHost($config['sslVerifyHost'] !== "false");
             }
 
             if (array_key_exists('sslVerifyPeer', $config) && !empty($config['sslVerifyPeer']))
             {
-                $pi->sslVerifyPeer = $config['sslVerifyPeer'] !== "false";
+                $pi->setSSLVerifyPeer($config['sslVerifyPeer'] !== "false");
             }
 
             if (array_key_exists('serviceAccount', $config) && !empty($config['serviceAccount']))
             {
-                $pi->serviceAccountName = $config['serviceAccount'];
+                $pi->setServiceAccountName($config['serviceAccount']);
             }
 
             if (array_key_exists('servicePass', $config) && !empty($config['servicePass']))
             {
-                $pi->serviceAccountPass = $config['servicePass'];
+                $pi->setServiceAccountPass($config['servicePass']);
             }
 
             if (array_key_exists('serviceRealm', $config) && !empty($config['serviceRealm']))
             {
-                $pi->serviceAccountRealm = $config['serviceRealm'];
+                $pi->setServiceAccountRealm($config['serviceRealm']);
             }
 
             if (array_key_exists('realm', $config) && !empty($config['realm']))
             {
-                $pi->realm = $config['realm'];
+                $pi->setRealm($config['realm']);
             }
 
             return $pi;
@@ -298,23 +298,23 @@ class Utils
         $config = $state['privacyidea:privacyidea'];
         $state['privacyidea:privacyidea:ui']['mode'] = "otp";
 
-        if (!empty($response->multiChallenge))
+        if (!empty($response->getMultiChallenge()))
         {
             // Authentication not complete, new challenges were triggered. Prepare the state for the next step.
             $triggeredTokens = $response->triggeredTokenTypes();
-            if (!empty($response->preferredClientMode))
+            if (!empty($response->getPreferredClientMode()))
             {
-                if ($response->preferredClientMode === "interactive")
+                if ($response->getPreferredClientMode() === "interactive")
                 {
                     $state['privacyidea:privacyidea:ui']['mode'] = "otp";
                 }
-                elseif ($response->preferredClientMode === "poll")
+                elseif ($response->getPreferredClientMode() === "poll")
                 {
                     $state['privacyidea:privacyidea:ui']['mode'] = "push";
                 }
                 else
                 {
-                    $state['privacyidea:privacyidea:ui']['mode'] = $response->preferredClientMode;
+                    $state['privacyidea:privacyidea:ui']['mode'] = $response->getPreferredClientMode();
                 }
                 Logger::debug("privacyIDEA: Preferred client mode: " . $state['privacyidea:privacyidea:ui']['mode']);
             }
@@ -322,7 +322,7 @@ class Utils
             $state['privacyidea:privacyidea:ui']['pushAvailable'] = in_array("push", $triggeredTokens);
             $state['privacyidea:privacyidea:ui']['otpAvailable'] = true;
 
-            $state['privacyidea:privacyidea:ui']['message'] = $response->messages;
+            $state['privacyidea:privacyidea:ui']['message'] = $response->getMessages();
 
             if (in_array("webauthn", $triggeredTokens))
             {
@@ -334,10 +334,10 @@ class Utils
                 $state['privacyidea:privacyidea:ui']['u2fSignRequest'] = $response->u2fSignRequest();
             }
 
-            $state['privacyidea:privacyidea']['transactionID'] = $response->transactionID;
+            $state['privacyidea:privacyidea']['transactionID'] = $response->getTransactionID();
 
             // Search for the image
-            foreach ($response->multiChallenge as $challenge)
+            foreach ($response->MultiChallenge() as $challenge)
             {
                 if (!empty($challenge->image))
                 {
@@ -360,7 +360,7 @@ class Utils
                 }
             }
         }
-        elseif ($response->value)
+        elseif ($response->getValue())
         {
             // Authentication successful. Finalize the authentication depending on method (AuthProc or AuthSource) and
             // write SSO specific data if enabled.
@@ -384,18 +384,18 @@ class Utils
                 PrivacyideaAuthSource::checkAuthenticationComplete($state, $response, $config);
             }
         }
-        elseif (!empty($response->errorCode))
+        elseif (!empty($response->getErrorCode()))
         {
             // privacyIDEA returned an error, prepare to display it
-            Logger::error("privacyIDEA: Error code: " . $response->errorCode . ", Error message: " . $response->errorMessage);
-            $state['privacyidea:privacyidea']['errorCode'] = $response->errorCode;
-            $state['privacyidea:privacyidea']['errorMessage'] = $response->errorMessage;
+            Logger::error("privacyIDEA: Error code: " . $response->getErrorCode() . ", Error message: " . $response->getErrorMessage());
+            $state['privacyidea:privacyidea']['errorCode'] = $response->getErrorCode();
+            $state['privacyidea:privacyidea']['errorMessage'] = $response->getErrorMessage();
         }
         else
         {
             // Unexpected response
-            Logger::error("privacyIDEA: " . $response->message);
-            $state['privacyidea:privacyidea']['errorMessage'] = $response->message;
+            Logger::error("privacyIDEA: " . $response->getMessage());
+            $state['privacyidea:privacyidea']['errorMessage'] = $response->getMessage();
         }
         return State::saveState($state, 'privacyidea:privacyidea');
     }
@@ -414,10 +414,10 @@ class Utils
     /**
      * Search for the configured headers in $_SERVER and return all found with their values.
      *
-     * @param $headers array List of headers to forward.
+     * @param array $headers List of headers to forward.
      * @return array Headers to forward with their values.
      */
-    public static function getHeadersToForward($headers)
+    public static function getHeadersToForward(array $headers): array
     {
         $cleanHeaders = str_replace(' ', '', $headers);
         $arrHeaders = explode(',', $cleanHeaders);
