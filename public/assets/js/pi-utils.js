5f7cdacd08bf2f5e543942fbe5e8141f89b2a57c
lukasmatusiewicz <77617779+lukasmatusiewicz@users.noreply.github.com>
2024-03-20 13:39:47 +0100

    Create pi-utils.js

---
+++ public/assets/js/pi-utils.js
@@ -0,0 +1,64 @@
+window.piGetValue = function getValue(id)
+{
+    const element = document.getElementById(id);
+    if (element === null)
+    {
+        console.log(id + " is null!");
+        return "";
+    }
+    else
+    {
+        return element.value;
+    }
+}
+
+window.piSetValue = function setValue(id, value)
+{
+    const element = document.getElementById(id);
+    if (element !== null)
+    {
+        element.value = value;
+    }
+    else
+    {
+        console.log(id + " is null!");
+    }
+}
+
+window.piDisableElement = function disableElement(id)
+{
+    const element = document.getElementById(id);
+    if (element !== null)
+    {
+        element.style.display = "none";
+    }
+    else
+    {
+        console.log(id + " is null!");
+    }
+}
+
+window.piEnableElement = function enableElement(id)
+{
+    const element = document.getElementById(id);
+    if (element !== null)
+    {
+        element.style.display = "initial";
+    }
+    else
+    {
+        console.log(id + " is null!");
+    }
+}
+
+window.piChangeMode = function changeMode(newMode)
+{
+    piSetValue("mode", newMode);
+    piSetValue("modeChanged", "1");
+    piSubmit();
+}
+
+window.piSubmit = function submitForm()
+{
+    document.forms["piLoginForm"].submit();
+}
