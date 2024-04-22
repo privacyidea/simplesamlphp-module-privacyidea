c60168b275a9184f4ee635ca6eea2e30db54ddd4
lukasmatusiewicz <77617779+lukasmatusiewicz@users.noreply.github.com>
2024-04-22 12:56:50 +0200

    Update PrivacyideaAuthSource.php

--- src/Auth/Source/PrivacyideaAuthSource.php
+++ src/Auth/Source/PrivacyideaAuthSource.php
@@ -191,7 +191,7 @@ class PrivacyideaAuthSource extends UserPassBase
                         Logger::error("privacyIDEA: Authentication flow not found in the config file. Please add the 'authenticationFlow' with one of the following values: 'sendPass', 'triggerChallenge' or 'separateOTP'. Until then, the login mask contains per default 1 user field and 1 pass field.");
                         try
                         {
-                            $response = $source->pi->validateCheck($username, $password, $headers);
+                            $response = $source->pi->validateCheck($username, $password, null, $headers);
                         }
                         catch (PIBadRequestException $e)
                         {
@@ -225,7 +225,7 @@ class PrivacyideaAuthSource extends UserPassBase
 
                             try
                             {
-                                $response = $source->pi->validateCheck($username, $password, $headers);
+                                $response = $source->pi->validateCheck($username, $password, null, $headers);
                             }
                             catch (PIBadRequestException $e)
                             {
@@ -237,7 +237,7 @@ class PrivacyideaAuthSource extends UserPassBase
                             Logger::error("privacyIDEA: Invalid authentication flow. Please set 'authenticationFlow' to one of the following values: 'sendPass', 'triggerChallenge' or 'separateOTP'. Fallback to default (sendPass)");
                             try
                             {
-                                $response = $source->pi->validateCheck($username, $password, $headers);
+                                $response = $source->pi->validateCheck($username, $password, null, $headers);
                             }
                             catch (PIBadRequestException $e)
                             {
